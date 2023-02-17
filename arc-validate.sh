e788750fa705bb5e0ffc534acf328fc3618cbf9d
Heinrich Lukas Weil <hl.weil@t-online.de>
2023-02-17 17:10:36 +0100

    switch output to JUnit format

--- arc-validate.sh
+++ arc-validate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -euxo pipefail
-dotnet run --project /opt/arc-validate/src/arc-validate.fsproj --nunit-summary arc-validate-results.xml
+dotnet run --project /opt/arc-validate/src/arc-validate.fsproj --junit-summary arc-validate-results.xml
