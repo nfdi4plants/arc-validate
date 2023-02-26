82d5e276d2dc25b354341e6f02844860c9932600
omaus <omaus@bio.uni-kl.de>
2023-02-26 10:01:56 +0100

    Add NUnit writer
    Code order reorganized; I also added a comment to declare that the code is not originally created by myself.

--- src/SummaryFileWriter.fs
+++ src/SummaryFileWriter.fs
@@ -8,6 +8,13 @@ open System.IO
 open System.Xml
 open System.Xml.Linq
 
+
+// delete when added to FSharpAux
+module String =
+    /// Returns a new string in which all leading and trailing occurrences of white-space characters from the current string are removed.
+    let trim (s : string) = s.Trim()
+
+
 let private expectoVersion = 
     // Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>()   // fails because of not having the .dll at hand
     let userDir = Environment.SpecialFolder.UserProfile |> Environment.GetFolderPath        // workaround which uses NuGet package version
@@ -17,6 +24,14 @@ let private expectoVersion =
         String.rev dir 
         |> String.takeWhile ((<>) '\\')
         |> String.rev
+
+
+
+
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// taken & modified from: https://github.com/haf/expecto
+// ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡
 
 let private assemblyName = Reflection.Assembly.GetEntryAssembly().GetName().Name
 
@@ -30,12 +45,7 @@ let private xmlSave fileName (doc : XDocument) =
     doc.Save writer
 
 
-// delete when added to FSharpAux
-module String =
-    /// Returns a new string in which all leading and trailing occurrences of white-space characters from the current string are removed.
-    let trim (s : string) = s.Trim()
-
-
+/// Generate test results using in a minimal JUnit schema.
 let writeJUnitSummary file (summary: Impl.TestRunSummary) =
 
     // junit does not have an official xml spec
@@ -87,3 +97,118 @@ let writeJUnitSummary file (summary: Impl.TestRunSummary) =
     element
     |> XDocument
     |> xmlSave file
+
+/// Generate test results using NUnit v2 schema.
+let writeNUnitSummary (file : string option) (summary : TestRunSummary) =
+    // v3: https://github.com/nunit/docs/wiki/Test-Result-XML-Format
+    // this impl is v2: http://nunit.org/docs/files/TestResult.xml
+    let totalTests = summary.errored @ summary.failed @ summary.ignored @ summary.passed
+    let testCaseElements =
+        totalTests
+        //|> Seq.sortByDescending (fun (_,test) -> test.result.order, test.duration.TotalSeconds)
+        |> Seq.map (fun (flatTest, test) ->
+                    
+            let fullnameString = 
+                flatTest.name 
+                |> List.fold (fun acc s -> acc + s + "; " ) "[ "
+                |> fun s -> s[.. String.length s - 3] + " ]"
+            let element = XElement(XName.Get "test-case", XAttribute(XName.Get "name", fullnameString))
+            let addAttribute name (content : string) = element.Add(XAttribute(XName.Get name, content))
+
+            //printfn $"test.result.order is {test.result.order}\ntest.result.tag is {test.result.tag}"
+
+            match test.result with
+            | Ignored _ -> "False"
+            | _ -> "True"
+            |> addAttribute "executed"
+
+            match test.result with
+            | Passed -> "Success"
+            | Error _
+            | Failed _ -> "Failure"
+            | Ignored _ -> "Ignored"
+            |> addAttribute "result"
+
+            match test.result with
+            | Passed -> addAttribute "success" "True"
+            | Error _
+            | Failed _ -> addAttribute "success" "False"
+            // Ignored tests are neither successful nor failed.
+            | Ignored _ -> ()
+
+            String.Format(CultureInfo.InvariantCulture, "{0:0.000}", test.duration.TotalSeconds)
+            |> addAttribute "time"
+
+            // TODO: implement it.
+            addAttribute "asserts" "0"
+
+            let failureNode = XElement(XName.Get "failure")
+
+            // Some more details that explain why a test was not executed.
+            match test.result with
+            | Passed -> ()
+            | Error e ->
+                failureNode.Add(XName.Get "message", XCData e.Message)
+                //failureNode.Add(XName.Get "stack-trace", XCData e.StackTrace)     // commented out to tackle unnecessary error stack trace in error message
+                element.Add failureNode
+            | Failed msg ->
+                // added to tackle unnecessary error stack trace in failure message
+                let eWithoutStackTrace =
+                    String.trim msg
+                    |> String.split '\n'
+                    |> Array.head
+                //failureNode.Add(XName.Get "message", XCData msg)
+                failureNode.Add(XName.Get "message", XCData eWithoutStackTrace)
+                element.Add failureNode
+            | Ignored msg -> element.Add(XElement(XName.Get "reason", XElement(XName.Get "message", XCData msg)))
+            element)
+    let d = DateTime.Now
+    let xAttr name data = XAttribute(XName.Get name, data)
+    let element =
+        XElement(XName.Get "test-results",
+            xAttr "date" (d.ToString("yyyy-MM-dd")),
+            xAttr "name" assemblyName,
+            xAttr "total" totalTests.Length,
+            xAttr "errors" summary.errored.Length,
+            xAttr "failures" summary.failed.Length,
+            xAttr "ignored" summary.ignored.Length,
+            xAttr "not-run" "0",
+            xAttr "inconclusive" "0",
+            xAttr "skipped" "0",
+            xAttr "invalid" "0",
+            xAttr "time" (d.ToString("HH:mm:ss")),
+            XElement(XName.Get "environment",
+                xAttr "expecto-version" expectoVersion,
+                xAttr "clr-version" Environment.Version,
+                xAttr "os-version" Environment.OSVersion.VersionString,
+                xAttr "platform" Environment.OSVersion.Platform,
+                xAttr "cwd" Environment.CurrentDirectory,
+                xAttr "machine-name" Environment.MachineName,
+                xAttr "user" Environment.UserName,
+                xAttr "user-domain" Environment.UserDomainName
+            ),
+            XElement(XName.Get "culture-info",
+                xAttr "current-culture", CultureInfo.CurrentCulture,
+                xAttr "current-uiculture", CultureInfo.CurrentUICulture
+            ),
+            XElement(XName.Get "test-suite",
+                xAttr "type" "Assembly",
+                xAttr "name" assemblyName,
+                xAttr "executed" "True",
+                xAttr "result" (if summary.successful then "Success" else "Failure"),
+                xAttr "success" (if summary.successful then "True" else "False"),
+                xAttr "time"
+                    (String.Format(CultureInfo.InvariantCulture,
+                        "{0:0.000}", summary.duration.TotalSeconds)),
+                xAttr "asserts" "0",
+                XElement(XName.Get "results", testCaseElements)
+            )
+        )
+    
+    let xDoc = XDocument element
+
+    match file with
+    | Some path -> xmlSave path xDoc
+    | None      -> ()
+
+    xDoc.ToString()
