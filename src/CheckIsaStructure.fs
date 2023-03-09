43838e9da295a90b339221d382d094791387df24
omaus <omaus@bio.uni-kl.de>
2023-03-09 12:45:17 +0100

    Specify registration checks
    Using Identifiers instead of whole objects due to implementation reasons of the objects. Also added some statements regarding proper usage.

--- src/CheckIsaStructure.fs
+++ src/CheckIsaStructure.fs
@@ -101,23 +101,35 @@ let private isSourceNameColumnPresentInSheet sheetName spreadsheet = isNodeColum
 /// Functions for checking the ISA structure of Studies.
 module Study =
 
+    /// Checks if an existing Study has a metadata section present.
+    // do this BEFORE checking for registration since Studies (with crucial Identifier) cannot be created from Study files without metadata section
+    let isMetadataSectionPresent spreadsheet = checkForMetadataSection spreadsheet "Study"
+
     /// Checks if all existing Studies are registered in the Investigation file and if all registered Studies in the Investigation file are present in the ARC.
     let checkStudiesRegistration studiesFromFiles inves =
         let studiesFromInves = 
             match inves.Studies with
             | Some sfis -> sfis
             | None      -> []
-        let studiesOutersect = List.outersect studiesFromInves studiesFromFiles
+        //let studiesOutersect = List.outersect studiesFromInves studiesFromFiles
+        // Simple outersect of both Study lists DOES NOT WORK right now due to the different ways that Studies are created from either files or the Investigation
+        // Therefore current workaround is: just comparing Study Identifiers
+        let idsSff = studiesFromFiles |> List.choose (fun s -> if s.Identifier.IsSome then s.Identifier else None)
+        let idsSfi = studiesFromInves |> List.choose (fun s -> if s.Identifier.IsSome then s.Identifier else None)
+        let studiesOutersect = List.outersect idsSff idsSfi
+        let studiesIntersect = List.intersect idsSff idsSfi
         {|
+            // true if all Studies present in the ARC filesystem are registered in the Investigation file
             AreStudiesRegistered    = studiesOutersect.Length <= 0
-            // studies that are present in the ARC filesystem but missing in the Investigation file
-            UnregisteredStudies     = studiesOutersect |> List.filter (fun so -> List.contains so studiesFromFiles)
-            // studies that are present in the Investigation file but missing in the ARC filesystem
-            MissingStudies          = studiesOutersect |> List.filter (fun so -> List.contains so studiesFromInves)
+            // Studies that are present in the ARC filesystem and registered in the Investigation file
+            RegisteredStudies       = studiesIntersect
+            // Studies that are present in the ARC filesystem but missing in the Investigation file
+            //UnregisteredStudies     = studiesOutersect |> List.filter (fun so -> List.contains so studiesFromFiles)
+            UnregisteredStudies     = studiesOutersect |> List.filter (fun so -> List.contains so idsSff)
+            // Studies that are present in the Investigation file but missing in the ARC filesystem
+            //MissingStudies          = studiesOutersect |> List.filter (fun so -> List.contains so studiesFromInves)
+            MissingStudies          = studiesOutersect |> List.filter (fun so -> List.contains so idsSfi)
         |}
-
-    /// Checks if an existing Study has a metadata section present.
-    let isMetadataSectionPresent spreadsheet = checkForMetadataSection spreadsheet "Study"
 
     /// 
     let isMetadataPresent study = ()
@@ -131,6 +143,10 @@ module Study =
 
 /// Functions for checking the ISA structure of Assays.
 module Assay =
+
+    /// Checks if an existing Assay has a metadata section present.
+    // do this BEFORE checking for registration since Assays (with crucial Identifier) cannot be created from Assay files without metadata section
+    let isMetadataSectionPresent assayPaths = checkForMetadataSection assayPaths "Assay"
 
     /// Checks if all existing Assays are registered in the Investigation file and if all registered Assays in the Investigation file are present in the ARC.
     let checkAssaysRegistration assaysFromPaths inves =
@@ -146,13 +162,23 @@ module Assay =
                     | Some ass -> ass
                     | None -> []
             )
-        let assaysOutersect = List.outersect assaysFromStudies assaysFromPaths
+        //let assaysOutersect = List.outersect assaysFromStudies assaysFromPaths
+        // Simple outersect of both Assay lists DOES NOT WORK right now due to the different ways that Assays are created from either files or the Investigation
+        // Therefore current workaround is: just comparing Assay Filenames (as representative of Assay Identifiers)
+        let idsAfp = assaysFromPaths |> List.choose (fun a -> if a.FileName.IsSome then a.FileName else None)
+        let idsAfi = assaysFromStudies |> List.choose (fun a -> if a.FileName.IsSome then a.FileName else None)
+        let assaysOutersect = List.outersect idsAfp idsAfi
+        let assaysIntersect = List.intersect idsAfp idsAfi
         {|
+            // true if all Studies present in the ARC filesystem are registered in the Investigation file
             AreAssaysRegistered = assaysOutersect.Length <= 0
-            UnregisteredAssays  = assaysOutersect |> List.filter (fun ao -> List.contains ao assaysFromPaths)
-            MissingAssays       = assaysOutersect |> List.filter (fun ao -> List.contains ao assaysFromStudies)
+            // Studies that are present in the ARC filesystem and registered in the Investigation file
+            RegisteredAssays    = assaysIntersect
+            // Studies that are present in the ARC filesystem but missing in the Investigation file
+            //UnregisteredAssays  = assaysOutersect |> List.filter (fun ao -> List.contains ao assaysFromPaths)
+            UnregisteredAssays  = assaysOutersect |> List.filter (fun ao -> List.contains ao idsAfp)
+            // Studies that are present in the Investigation file but missing in the ARC filesystem
+            //MissingAssays       = assaysOutersect |> List.filter (fun ao -> List.contains ao assaysFromStudies)
+            MissingAssays       = assaysOutersect |> List.filter (fun ao -> List.contains ao idsAfi)
         |}
-
-    /// Checks if an existing Assay has a metadata section present.
-    let isMetadataSectionPresent assayPaths = checkForMetadataSection assayPaths "Assay"
 
