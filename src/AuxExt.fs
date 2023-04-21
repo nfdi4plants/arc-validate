e95dc39462ff942970b13c4bdd4ebfa93a271fb6
omaus <omaus@bio.uni-kl.de>
2023-04-21 16:43:00 +0200

    Add module of unspecific auxiliary functions

---
+++ src/AuxExt.fs
@@ -0,0 +1,30 @@
+﻿[<AutoOpen>]
+module AuxExt
+
+open System
+open FSharpAux
+open FsSpreadsheet
+
+
+
+module String =
+
+    let isNullOrWhiteSpace (str : string) =
+        System.String.IsNullOrWhiteSpace str
+
+    /// Checks if an input string option is None or, if it is Some, null, empty or consisting only of whitespaces.
+    let isNoneOrWhiteSpace str =
+        match str with
+        | None -> true
+        | Some s -> String.IsNullOrWhiteSpace s
+
+    /// Checks if a string is a filepath.
+    let isFilepath str =
+        (String.contains "/" str || String.contains "\\" str) &&
+        IO.Path.GetExtension str <> ""
+
+    /// Splits an file address string into a triple in the form of `sheetName * rowNumber * columnNumber`.
+    let splitAddress str =
+        let sheetName, res = String.split '!' str |> fun arr -> arr[0], arr[1]
+        let adr = FsAddress res
+        sheetName, adr.RowNumber, adr.ColumnNumber |> uint |> CellReference.indexToColAdress
