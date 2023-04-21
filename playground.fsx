1196f1e9229948ec56915f146db811e2f3799f34
omaus <omaus@bio.uni-kl.de>
2023-04-21 11:19:36 +0200

    Add module containing functions about validating CvBases

--- playground.fsx
+++ playground.fsx
@@ -26,36 +26,7 @@ let testCvP3 = CvParam("ISA:0001", "Sample Name", "ISA", ParamValue.Value "", ad
 testCvP["Address"] :> IParamBase |> ParamBase.getValue
 testCvP :> IParamBase |> ParamBase.getValue
 
-/// Returns a CvParam's cell address (in the form of `<sheetname>!<column letter><row number>`, e.g. `sheet1!A1`) for a given column
-/// type if `predicate` returns true. Else returns None.
-let returnCellAddressWhen predicate columnType (cvParam : CvParam) =
-    match BuildingBlockType.tryOfString (cvParam :> ICvBase).Name with
-    | Some x when columnType x ->
-        let isEmpty = 
-            cvParam
-            :> IParamBase 
-            |> ParamBase.getValue 
-            |> string
-            |> predicate
-        if isEmpty then Some (cvParam["Address"] :> IParamBase |> ParamBase.getValue |> string)
-        else None
-    | _ -> None
 
-let containsFilePath cvParam =
-    returnCellAddressWhen (Ont)
-
-/// Returns all Source Name cells with an empty value.
-let returnEmptyInputCells cvParams = 
-    cvParams |> List.choose (returnCellAddressWhen String.isNullOrEmpty (fun c -> c.IsInputColumn))
-
-/// Returns all Sample Name / Raw Data File / Derived Data File cells with an empty value.
-let returnEmptyOutputCells cvParams = 
-    cvParams |> List.choose (returnCellAddressWhen String.isNullOrEmpty (fun c -> c.IsOutputColumn))
-
-/// Returns all RawDataFile names, DerivedDataFile names and ProtocolREF filenames that do not exist in the ARC.
-let returnNonExistentFileNames cvParams =
-    // root path to ARC folder must be part of the string at this point already
-    cvParams |> List.choose (returnCellAddressWhen (System.IO.File.Exists >> not) (fun c -> c = RawDataFile || c = DerivedDataFile || c = ProtocolREF))
 
 returnEmptyInputCells [testCvP; testCvP2; testCvP3]
 returnEmptyOutputCells [testCvP; testCvP2; testCvP3]
---
+++ src/ValidateCvBase.fs
@@ -0,0 +1,36 @@
+﻿module ValidateCvBase
+
+open ArcGraphModel
+open ArcGraphModel.ArcType
+open FSharpAux
+
+/// Returns a CvParam's cell address (in the form of `<sheetname>!<column letter><row number>`, e.g. `sheet1!A1`) for a given column
+/// type if `predicate` returns true. Else returns None.
+let returnCellAddressWhen predicate columnType (cvParam : CvParam) =
+    match BuildingBlockType.tryOfString (cvParam :> ICvBase).Name with
+    | Some x when columnType x ->
+        let isEmpty = 
+            cvParam
+            :> IParamBase 
+            |> ParamBase.getValue 
+            |> string
+            |> predicate
+        if isEmpty then Some (cvParam["Address"] :> IParamBase |> ParamBase.getValue |> string)
+        else None
+    | _ -> None
+
+let containsFilePath cvParam =
+    returnCellAddressWhen (Ont)
+
+/// Returns all Source Name cells with an empty value.
+let returnEmptyInputCells cvParams = 
+    cvParams |> List.choose (returnCellAddressWhen String.isNullOrEmpty (fun c -> c.IsInputColumn))
+
+/// Returns all Sample Name / Raw Data File / Derived Data File cells with an empty value.
+let returnEmptyOutputCells cvParams = 
+    cvParams |> List.choose (returnCellAddressWhen String.isNullOrEmpty (fun c -> c.IsOutputColumn))
+
+/// Returns all RawDataFile names, DerivedDataFile names and ProtocolREF filenames that do not exist in the ARC.
+let returnNonExistentFileNames cvParams =
+    // root path to ARC folder must be part of the string at this point already
+    cvParams |> List.choose (returnCellAddressWhen (System.IO.File.Exists >> not) (fun c -> c = RawDataFile || c = DerivedDataFile || c = ProtocolREF))
