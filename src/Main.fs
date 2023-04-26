960b6d24feede23a8a3cbd948f839299e054a894
omaus <omaus@bio.uni-kl.de>
2023-04-26 12:02:27 +0200

    Add python badge script to solution

--- src/Main.fs
+++ src/Main.fs
@@ -7,8 +7,5 @@ let outputPath = System.IO.Path.Combine(ArcPaths.inputPath, "arc-validate-result
 
 [<EntryPoint>]
 let main argv =
-    printfn "%s" ArcPaths.inputPath
-    //Tests.runTestsInAssembly defaultConfig [|yield! argv; FilesystemStructure.Paths.inputPath|]
-    //Tests.runTestsInAssembly defaultConfig argv
     performTest (testList "ARCTests" [ValidateArc.filesystem; ValidateArc.isaTests]) |> Expecto.writeJUnitSummary outputPath
     0
