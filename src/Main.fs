6707aff489a62b8f88248c84e9027d4f38c29255
Kevin Schneider <schneider.kev@outlook.de>
2023-04-27 10:30:05 +0200

    1st try "hotfix" for gitlab CI

--- src/Main.fs
+++ src/Main.fs
@@ -3,8 +3,8 @@
 open Expecto
 
 
-let outputPath = System.IO.Path.Combine(ArcPaths.inputPath, "arc-validate-result.xml")
-
+//let outputPath = System.IO.Path.Combine(ArcPaths.inputPath, "arc-validate-result.xml")
+let outputPath = "arc-validate-result.xml"
 [<EntryPoint>]
 let main argv =
     performTest (testList "ARCTests" [ValidateArc.filesystem; ValidateArc.isaTests]) |> Expecto.writeJUnitSummary outputPath
