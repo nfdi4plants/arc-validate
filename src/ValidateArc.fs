5a47eec7fccac035e7574fa5ba50ffbcafaefaa1
Oliver Maus <oliver.maus@eclipso.de>
2023-04-28 19:41:50 +0200

    Add module for getting information from the ARC

--- src/ValidateArc.fs
+++ src/ValidateArc.fs
@@ -6,56 +6,9 @@ open CheckIsaStructure
 open Expecto
 open ArcGraphModel
 open ArcGraphModel.IO
-open FsSpreadsheet
-open FsSpreadsheet.ExcelIO
 open ErrorMessage.FailStrings
-open System.IO
 open FSharpAux
-open CvTokens
-
-
-let invWb = FsWorkbook.fromXlsxFile investigationPath
-let invWorksheet = 
-    let ws = 
-        invWb.GetWorksheets()
-        |> List.find (fun ws -> ws.Name = "isa_investigation")
-    ws.RescanRows()
-    ws
-
-let invPathCvP = CvParam(Terms.filepath, ParamValue.Value investigationPath)
-
-let invTokens = 
-    let it = Worksheet.parseRows invWorksheet
-    List.iter (fun cvb -> CvAttributeCollection.tryAddAttribute invPathCvP cvb |> ignore) it
-    it
-
-let invContainers = 
-    invTokens
-    |> TokenAggregation.aggregateTokens 
-
-let invStudies =
-    invContainers
-    |> Seq.choose CvContainer.tryCvContainer
-    |> Seq.filter (fun cv -> CvBase.equalsTerm Terms.study cv)
-
-let invContacts =
-    invContainers
-    |> Seq.choose CvContainer.tryCvContainer
-    |> Seq.filter (fun cv -> CvBase.equalsTerm Terms.person cv && CvContainer.isPartOfInvestigation cv)
-    //|> Seq.filter (fun cv -> CvBase.equalsTerm Terms.person cv && CvContainer.tryGetAttribute (CvTerm.getName Terms.investigation) cv |> Option.isSome)
-
-let invStudiesPathsAndIds =
-    invStudies
-    |> Seq.map (
-        fun cvc ->
-            CvContainer.tryGetSingleAs<IParam> "File Name" cvc
-            |> Option.map (
-                Param.getValueAsString 
-                >> fun s -> Path.Combine(ArcPaths.studiesPath,s)
-            ),
-            CvContainer.tryGetSingleAs<IParam> "identifier" cvc
-            |> Option.map Param.getValueAsString
-    )
+open InformationExtraction
 
 
 [<Tests>]
