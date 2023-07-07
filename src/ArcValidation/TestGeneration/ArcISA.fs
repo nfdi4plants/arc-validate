7988402b7b659bf78973c5a149c9640ed530d7da
omaus <omaus@bio.uni-kl.de>
2023-07-07 11:55:05 +0200

    Add optional person name in person validation case name

--- src/ArcValidation/TestGeneration/ArcISA.fs
+++ src/ArcValidation/TestGeneration/ArcISA.fs
@@ -13,6 +13,7 @@ module ISA =
     open ErrorMessage.FailStrings
     open FSharpAux
     open System.IO
+    open CvTokenHelperFunctions
 
     let generateISATests (arcConfig: ArcConfig) =
 
@@ -28,8 +29,14 @@ module ISA =
                         |> List.ofSeq
                         |> List.mapi (
                             fun i p ->
+                                // try get person name if possible to display more precise test case name
+                                let optName = 
+                                    let optFN = CvContainer.tryGetPropertyStringValue "first name" p    |> Option.defaultValue "?"
+                                    let optLN = CvContainer.tryGetPropertyStringValue "last name" p     |> Option.defaultValue "?"
+                                    let optN = $"{optFN} {optLN}"
+                                    if optN = "? ?" then "(n/a)" else optN
                                 // Validate the sufficiency of a Person in Investigation Contacts section (a Person is sufficient when first and last name, and email address are present):
-                                testList $"Person{i + 1}" [
+                                testList $"Person{i + 1} [{optName}]" [
                                     testCase "First name"       <| fun () -> Validate.CvBase.Person.firstName       p |> throwError FilesystemEntry.isPresent
                                     testCase "Last name"        <| fun () -> Validate.CvBase.Person.lastName        p |> throwError FilesystemEntry.isPresent
                                     testCase "Email address"    <| fun () -> Validate.CvBase.Person.emailAddress    p |> throwError FilesystemEntry.isPresent
