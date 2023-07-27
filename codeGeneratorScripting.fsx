27ea89b43b2bc5922851c129c5c77616392be6e2
Oliver Maus <oliver.maus@eclipso.de>
2023-07-27 15:11:23 +0200

    Update code generation ideas

--- codeGeneratorScripting.fsx
+++ codeGeneratorScripting.fsx
@@ -6,12 +6,14 @@ open FSharpAux
 
 let eco = OboOntology.fromFile true @"C:\Repos\nfdi4plants\arc-validate\ErrorClassOntology.obo"
 
-let spaßTerms = [
-    OboTerm.Create("spaß:00000000", Name = "spaß")
+let testTerms = [
+    OboTerm.Create("test:00000000", Name = "test")
+    OboTerm.Create("test:00000001", Name = "test2a", IsA = ["test:00000000"])
+    OboTerm.Create("test:00000002", Name = "test3", IsA = ["test:00000001"])
+    OboTerm.Create("test:00000003", Name = "test2b", IsA = ["test:00000000"])
 ]
 
-let spaßOntology =
-    OboOntology.create spaßTerms []
+let testOntology = OboOntology.create testTerms []
 
 eco.Terms.Head
 let no5 = eco.Terms[5]
@@ -20,11 +22,45 @@ no5.Id
 
 let oal = eco.GetParentOntologyAnnotations(no5.Id)
 
+let getParents (ontology : OboOntology) (term : OboTerm) =
+    ontology.GetParentOntologyAnnotations(term.Id)
+    |> List.skip 1      // 1st item is always the term itself which we don't need here anymore
+    |> List.map (fun oa -> ontology.GetTerm(oa.TermAccessionString.ToString()))
+
+eco.GetChildOntologyAnnotations(no5.Id)
+eco.GetChildOntologyAnnotations(eco.Terms[1].Id)
+testOntology.GetChildOntologyAnnotations(testTerms.Head.Id)
+
+let getChildren (ontology : OboOntology) (term : OboTerm) =
+    ontology.GetChildOntologyAnnotations(term.Id)
+
+let parents = getParents eco no5
+let 
+
+let templateModuleString = """module <name> =
+
+"""
+
+let templateTypeString = """type <name> =
+    static member name = "<name>"
+"""
+
+
+
+
+
+
+
 // parents vs isA: parents is all isAs or partOfs recursively traced to the last ancestor while isA (or partOf in relationships list) is only the first parent
 
+
+
+// XXXXXXXXXXX
+// DEPRECATED!
+// XXXXXXXXXXX
+
 [<Literal>]
-let templateTermString = """
-class <name>(value : string) =
+let templateTermString = """class <name>(value : string) =
     static member parents = <parents>
     static member isA = <isA>
     static member id = <id>
@@ -49,13 +85,6 @@ class <name>(value : string) =
     member this.Value = value
 """
 
-let getParents (ontology : OboOntology) (term : OboTerm) =
-    ontology.GetParentOntologyAnnotations(term.Id)
-    |> List.skip 1      // 1st item is always the term itself which we don't need here anymore
-    |> List.map (fun oa -> ontology.GetTerm(oa.TermAccessionString.ToString()))
-
-let parents = getParents eco no5
-
 let foldListIntoString list =
     list
     |> List.foldi (
@@ -67,8 +96,42 @@ let foldListIntoString list =
 
 ["a1"; "a2"] |> foldListIntoString |> printfn "%s"
 
+let myBool = false
+myBool.ToString()
+
+let boolString str =
+    if str then "true" else "false"
+
+boolString myBool
+
+let oboTermCreateString = """OboTerm.Create(<id>, Name = <name>, IsAnonymous = <isAnonymous>, AltIds = <>, Definition = <definition>, Comment = <comment>, Subsets = <subsets>, Synonyms = <synonyms>, Xrefs = <xrefs>, IsA = <isA>, IntersectionOf = <intersectionOf>, UnionOf = <unionOf>, DisjointFrom = <disjointFrom>, Relationships = <relationships>, IsObsolete = <isObsolete>, Replacedby = <replacedBy>, Consider = <>, PropertyValues = <propertyValues>, BuiltIn = <builtIn>, CreatedBy = <createdBy>, CreationDate = <creationDate>)"""
+
+let internal replaceTermPlaceholder template (term : OboTerm) =
+    template
+    |> String.replace "<name>" term.Name
+    |> String.replace "<id>" term.Id
+    |> String.replace "<definition>" term.Definition
+    |> String.replace "<comment>" term.Comment
+    |> String.replace "<createdBy>" term.CreatedBy
+    |> String.replace "<creationDate>" term.CreationDate
+    |> String.replace "<isAnonymous>" (boolString term.IsAnonymous)
+    |> String.replace "<isObsolete>" (boolString term.IsObsolete)
+    |> String.replace "<builtIn>" (boolString term.BuiltIn)
+    |> String.replace "<isA>" (foldListIntoString term.IsA)
+    |> String.replace "<subsets>" (foldListIntoString term.Subsets)
+    |> String.replace "<synonyms>" (foldListIntoString term.Synonyms)
+    |> String.replace "<xrefs>" (foldListIntoString term.Xrefs)
+    |> String.replace "<intersectionOf>" (foldListIntoString term.IntersectionOf)
+    |> String.replace "<unionOf>" (foldListIntoString term.UnionOf)
+    |> String.replace "<disjointFrom>" (foldListIntoString term.DisjointFrom)
+    |> String.replace "<relationships>" (foldListIntoString term.Relationships)
+    |> String.replace "<replacedBy>" (foldListIntoString term.Replacedby)
+    |> String.replace "<propertyValues>" (foldListIntoString term.PropertyValues)
+
+let solidifyOboTerm term =
+    replaceTermPlaceholder oboTermCreateString term
+
 let insertIntoTemplate ontology term =
     let parents = getParents ontology term
-    templateTermString
+    replaceTermPlaceholder templateTermString term
     |> String.replace "<parents>" ""
-    |> String.replace "<isA>" (foldListIntoString term.IsA)
