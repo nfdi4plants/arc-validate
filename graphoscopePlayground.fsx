12f38298cf51a07d6c881e53d9b46c825870e67e
omaus <omaus@bio.uni-kl.de>
2023-07-31 00:49:16 +0200

    Add Graphoscope tries

--- graphoscopePlayground.fsx
+++ graphoscopePlayground.fsx
@@ -16,24 +16,63 @@ let testTerms = [
 
 let testOntology = OboOntology.create testTerms []
 
-let graph2 = FGraph.empty
+let graph2 = FGraph.empty<string,string,string>
 
-let nodes2 = 
-    testOntology.Terms 
-    |> List.map (fun t -> FGraph.Node.add t.Id t.Name graph2)
+testOntology.Terms 
+|> List.map (fun t -> FGraph.Node.add t.Id t.Name graph2)
 
-let edges2 =
-    testOntology.Terms 
+testOntology.Terms 
+|> List.collect (
+    fun t -> 
+        t.IsA
+        |> List.map (
+            fun isA -> FGraph.Edge.add t.Id isA "is_a" graph2
+        )
+)
+
+let result = Algorithms.BFS.Compute("test:00000003", graph2) |> List.ofSeq
+let result2 = Algorithms.BFS.Compute("test:00000002", graph2) |> List.ofSeq
+
+//let graph3 = FGraph.ofSeq (testTerms |> List.map (fun t -> t.))
+
+let errorOntology = OboOntology.fromFile false (System.IO.Path.Combine(__SOURCE_DIRECTORY__, "ErrorClassOntology.obo"))
+
+let graph3 = 
+    let g = FGraph.empty<string,string,string>
+    errorOntology.Terms 
+    |> List.map (fun t -> FGraph.Node.add t.Id t.Name g)
+    |> ignore
+    errorOntology.Terms 
     |> List.collect (
         fun t -> 
             t.IsA
             |> List.map (
-                fun isA -> FGraph.Edge.add t.Id isA "is_a" graph2
+                fun isA -> FGraph.Edge.add t.Id isA "is_a" g
             )
     )
+    |> ignore
+    g
 
-let result = Algorithms.BFS.Compute("test:00000003", graph2)
+let result3 = Algorithms.BFS.Compute("DPEO:00000018", graph3) |> List.ofSeq
+let result4 = Algorithms.BFS.Compute("DPEO:00000000", graph3) |> List.ofSeq
 
-FGraph.ofSeq
+let graph4 = 
+    let g = FGraph.empty<string,string,string>
+    errorOntology.Terms 
+    |> List.map (fun t -> FGraph.Node.add t.Id t.Name g)
+    |> ignore
+    errorOntology.Terms 
+    |> List.collect (
+        fun t -> 
+            t.IsA
+            |> List.map (
+                fun isA -> FGraph.Edge.add isA t.Id "has_a" g
+            )
+    )
+    |> ignore
+    g
 
-result |> List.ofSeq
+graph4 |> FGraph.mapContexts (fun t -> t)
+
+let result5 = Algorithms.BFS.Compute("DPEO:00000018", graph4) |> List.ofSeq
+let result6 = Algorithms.BFS.Compute("DPEO:00000000", graph4) |> List.ofSeq
