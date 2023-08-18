5e8909ebfe9409258da523e0d1079c7082ed373a
Kevin Schneider <schneider.kev@outlook.de>
2023-08-18 17:13:47 +0200

    Add top-level API for installing packages and updating package index

--- src/ARCValidationPackages/Config.fs
+++ src/ARCValidationPackages/Config.fs
@@ -29,11 +29,42 @@ type Config = {
             configFilePath = Defaults.CONFIG_FILE_PATH()
         )
 
+    static member indexContainsPackage (packageName: string) (config: Config) =
+        config.PackageIndex |> Array.exists (fun package -> package.Name = packageName)
+
+    static member tryGetIndexedPackageByName (packageName: string) (config: Config) =
+        if Config.indexContainsPackage packageName config then
+            Some (config.PackageIndex |> Array.find (fun package -> package.Name = packageName))
+        else
+            None
+
+    static member getIndexedPackageByName (packageName: string) (config: Config) =
+        if Config.indexContainsPackage packageName config then
+            config.PackageIndex |> Array.find (fun package -> package.Name = packageName)
+        else
+            failwithf "Package %s not found in index" packageName
+
+    static member withIndex (index: ValidationPackageIndex []) (config: Config) =
+        {
+            config with
+                PackageIndex = index
+        }
+
+    static member exists (?Path: string) =
+        let path = defaultArg Path (Defaults.CONFIG_FILE_PATH())
+        File.Exists(path)
+
     static member read (?Path: string) =
         let path = defaultArg Path (Defaults.CONFIG_FILE_PATH())
         path
         |> File.ReadAllText
         |> fun jsonString -> JsonSerializer.Deserialize<Config>(jsonString, Defaults.SERIALIZATION_OPTIONS)
+
+    static member get (?Path: string) =
+        if Config.exists(?Path = Path) then
+            Config.read()
+        else
+            Config.initDefault()
 
     static member write (?Path: string) =
         fun (config: Config) ->
---
+++ src/ARCValidationPackages/TopLevelAPI.fs
@@ -0,0 +1,90 @@
+﻿namespace ARCValidationPackages
+
+open System.IO
+
+module Errors =     
+
+    type UpdateIndexError =
+    | DownloadError of msg: string
+
+    type PackageInstallError = 
+    | PackageNotFound of package: string
+    | DownloadError of package: string * msg: string
+
+    type PackageAPIError =
+        | UpdateIndexError of UpdateIndexError
+        | PackageInstallError of PackageInstallError
+
+module API = 
+
+    open Errors
+
+    let saveAndCachePackage (cache: PackageCache) indexedPackage =
+        let package = ARCValidationPackage.ofPackageIndex indexedPackage
+        try 
+            GitHubAPI.downloadPackageScript(indexedPackage)
+            |> fun script -> 
+                File.WriteAllText(
+                    package.LocalPath,
+                    script
+                )
+
+            cache
+            |> PackageCache.addPackage package
+            |> PackageCache.write()
+
+            Ok ($"installed package {package.Name} at {package.LocalPath}")
+                
+        with e ->
+            Error (PackageInstallError.DownloadError(package.Name, e.Message))
+
+    let updateIndex (config: Config) : Result<Config, UpdateIndexError> =
+        try 
+            let updatedIndex = GitHubAPI.getPackageIndex()
+
+            let updatedConfig = 
+                config
+                |> Config.withIndex updatedIndex
+
+            updatedConfig |> Config.write()
+
+            Ok updatedConfig
+
+        with e ->
+            Error (UpdateIndexError.DownloadError(e.Message))
+
+    let installPackage (verbose: bool) (config: Config) (cache: PackageCache) (packageName: string) : Result<string, PackageInstallError> =
+
+        if (Config.indexContainsPackage packageName config) then
+            // package exists on the local index
+            let indexedPackage = Config.getIndexedPackageByName packageName config
+
+            if cache.ContainsKey(packageName) then
+                //already cached -> update index and check if newer package is available
+                let cachedPackage = cache.[packageName]
+
+                if verbose then printfn $"package {packageName} is already cached locally from {cachedPackage.CacheDate}"
+                if verbose then printfn $"updating package index and looking for a newer version..."
+
+                match updateIndex config with
+                | Ok updatedConfig -> 
+                    let updatedIndexPackage = Config.getIndexedPackageByName packageName updatedConfig
+                    if updatedIndexPackage.LastUpdated > indexedPackage.LastUpdated then
+                        // package on remote index is newer, download package and cache.
+                        if verbose then printfn $"package {packageName} is available in a newer version({updatedIndexPackage.LastUpdated} vs {indexedPackage.LastUpdated}). downloading..."
+                        saveAndCachePackage cache updatedIndexPackage
+
+                    else
+                        // package is installed with latest version
+                        Ok ($"package {indexedPackage.Name} is already installed with the latest version.")
+
+                | Error e -> 
+                    Error (PackageInstallError.DownloadError(packageName, $"failed to update package index: {e}"))
+
+            else 
+                // not cached -> download and cache
+                saveAndCachePackage cache indexedPackage
+
+        else    
+            // package does not exists on the local index
+            Error (PackageNotFound packageName)
