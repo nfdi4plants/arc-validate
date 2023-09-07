7cbacf4382a0a849ead8cc7f1d30281fc65801db
Oliver Maus <oliver.maus@eclipso.de>
2023-09-07 10:15:17 +0200

    Make empty ORCID viable

--- src/ArcValidation/Validate/NonCritical/ValidateCvBase.fs
+++ src/ArcValidation/Validate/NonCritical/ValidateCvBase.fs
@@ -17,8 +17,9 @@ module CvBase =
         /// Validates a person's ORCID.
         let orcid<'T when 'T :> CvContainer> (personCvContainer : 'T) = 
             let orcidProperty = CvContainer.tryGetPropertyStringValue "<  ORCID>" personCvContainer
+            printfn "ORCID property: %A" orcidProperty
             let err = Error (ErrorMessage.FilesystemEntry.createFromCvParam personCvContainer)
             match orcidProperty with
-            | s when String.isNoneOrWhiteSpace s -> err
+            //| s when String.isNoneOrWhiteSpace s -> err
             | o when InternalUtils.Orcid.checkValid o.Value |> not -> err
             | _ -> Success
