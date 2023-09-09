2e583e30330e1522afd41af29e081b2775f9a3a2
muehlhaus <muehlhaus@rptu.de>
2023-09-09 16:40:59 +0200

    Add ArcExpect-Test

--- src/ArcValidation/InternalUtils.fs
+++ src/ArcValidation/InternalUtils.fs
@@ -56,7 +56,43 @@ module InternalUtils =
 
 
 
+    /// Functions to work with ORCID numbers.
+    module Orcid =
 
+        /// Calculates the checksum digit of an ORCID.
+        /// The calculated checksum digit must match the last character of the given ORCID.
+        /// 
+        /// Input parameter "digits" must be the full ORCID to check but already without all hyphens excluded, e.g. `1111222233334444`.
+        // modified for F# from: https://support.orcid.org/hc/en-us/articles/360006897674-Structure-of-the-ORCID-Identifier
+        let checksum (digits : string) = 
+            let rec loop i total =
+                if i < digits.Length - 1 then
+                    let digit = Char.GetNumericValue digits[i] |> int
+                    loop (i + 1) ((total + digit) * 2)
+                else total
+            let remainder = (loop 0 0) % 11
+            let result = (12 - remainder) % 11
+            if result = 10 then 'X' else string result |> char
+
+        /// Checks if the given ORCID digits are in range.
+        /// Current range is 0000000150000007 to 0000000350000001 and 0009000000000000 to 0009001000000000 (as of July 2023).
+        /// 
+        /// Input parameter "digits" must be the full ORCID to check but already without all hyphens excluded, e.g. `1111222233334444`.
+        // source: https://support.orcid.org/hc/en-us/articles/360006897674-Structure-of-the-ORCID-Identifier
+        let checkRange (digits : string) =
+            let inNums = 
+                String.replace "X" "9" digits   // debatable if sufficient. Best approach would be to raise the digit before by 1 (recursively, if it'd be 9 a.s.o.)
+                |> int64
+            (inNums >= 150000007L && inNums <= 350000001L) || (inNums >= 9000000000000L && inNums <= 9001000000000L)
+
+        /// Checks if a given string is a valid ORCID.
+        /// 
+        /// Checks if the ORCID is in current number range and has a valid checksum digit.
+        let checkValid (input : string) =            
+            let orcid = System.Text.RegularExpressions.Regex("^\d{4}-\d{4}-\d{4}-\d{3}[0-9X]$")
+            let isNum = orcid.Match(input).Success
+            let noHyphens = String.replace "-" "" input
+            isNum && checkRange noHyphens && checksum noHyphens = noHyphens[noHyphens.Length - 1]
 
     type OboTerm with
 
--- src/ArcValidation/TestGeneration/Critical/ArcISA.fs
+++ src/ArcValidation/TestGeneration/Critical/ArcISA.fs
@@ -20,3 +20,80 @@ module ISA =
                 ]
             ]
         ]
+
+
+//open Expecto
+//open ParamCollection
+//open IsaTerms
+//open IsaTerms.Investigation
+
+//let filePathCv = CvParam(IoTerms.FilePath,"/isa.investigation (Venni).xlsx") 
+
+//let isaInvestigationTokens = 
+//    __SOURCE_DIRECTORY__ + "/isa.investigation (Venni).xlsx"
+//    |> ARCTokenization.Investigation.parseMetadataSheetFromFile
+//    |> List.map (fun iparam -> 
+//        match CvParam.tryAddAttribute filePathCv iparam with
+//        | _ -> iparam
+//        )
+
+
+
+//let simpleTest =
+//  test "A simple test" {
+//    match tryFindby Contacts.``Investigation Person Email`` isaInvestigationTokens with
+//    | Some cvp ->
+//        ArcExpect.ByValue.notEmpty cvp
+//        ArcExpect.ByValue.isMatch StringValidationPattern.email  cvp   
+//    | None -> ()
+//  }
+
+
+
+ 
+
+
+
+//let emailTests=        
+//    testList "Email required"         
+//        [
+//        for cvp in findAllby Contacts.``Investigation Person Email`` isaInvestigationTokens do            
+//            ArcExpect.test TestID.Guid
+//                {
+                                        
+
+//                    // ArcExpect.either 
+//                    //     { ArcExpect.ByValue.notEmpty cvp }
+//                    // ``or`` ArcExpect.either
+//                    //     {ArcExpect.ByValue.isMatch @"^[^@\s]+@[^@\s]+\.[^@\s]+$" cvp }
+                    
+                     
+//                    //this (ArcExpect.ByValue.notEmpty cvp)
+
+//                    if not <| CvParamExtensions.isEmpty cvp then
+//                        ArcExpect.ByValue.isMatch @"^[^@\s]+@[^@\s]+\.[^@\s]+$"  cvp   
+
+                    
+//                    // either                   
+//                    //     <| fun () -> ArcExpect.ByValue.notEmpty cvp
+//                    //     <| fun () -> ArcExpect.ByValue.isMatch @"^[^@\s]+@[^@\s]+\.[^@\s]+$"  cvp
+
+
+//                    // ArcExpect.ByValue.notEmpty cvp
+                    
+//                    // try
+//                    //     ArcExpect.ByValue.notEmpty cvp
+//                    // with
+//                    // | ex1 -> 
+//                    //     try
+//                    //         ArcExpect.ByValue.isMatch @"^[^@\s]+@[^@\s]+\.[^@\s]+$"  cvp   
+//                    //     with
+//                    //     | ex2 ->                          
+//                    //         Expecto.Tests.failtestNoStackf "Probleme is either\n %s \n or\n %s" ex1.Message ex2.Message
+
+//                }
+//        ]  
+
+
+
+//runTestsWithCLIArgs [] [||] emailTests
