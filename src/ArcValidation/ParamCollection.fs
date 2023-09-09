2b2b42c8a4c1577574690b8b52228e8125cf9f62
muehlhaus <muehlhaus@rptu.de>
2023-09-09 15:54:29 +0200

    Add find CvTerm in ParamCollection

---
+++ src/ArcValidation/ParamCollection.fs
@@ -0,0 +1,23 @@
+﻿namespace ArcValidation
+
+module ParamCollection =
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cvt"> </param>
+    /// <param name="source"> </param>    
+    let tryFindby (cvt:CvTerm) (source : seq<IParam>) =
+        source
+        |> Seq.choose Param.tryCvParam
+        |> Seq.tryFind (fun param -> CvParam.getTerm param = cvt)
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cvt"> </param>
+    /// <param name="source"> </param>          
+    let findAllby (cvt:CvTerm) (source : seq<IParam>) =
+        source
+        |> Seq.choose Param.tryCvParam
+        |> Seq.filter (fun param -> CvParam.getTerm param = cvt)
