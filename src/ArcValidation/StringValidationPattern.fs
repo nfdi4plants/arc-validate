1bd32efe7bf24f0fed2c98ab8870d00727bd151a
muehlhaus <muehlhaus@rptu.de>
2023-09-09 16:06:19 +0200

    Add Email StringValidationPattern

---
+++ src/ArcValidation/StringValidationPattern.fs
@@ -0,0 +1,10 @@
+﻿namespace ArcValidation
+
+module StringValidationPattern =
+
+    open System
+    open System.Text.RegularExpressions
+    
+    let email = Regex(@"^[^@\s]+@[^@\s]+\.[^@\s]+$")
+
+
