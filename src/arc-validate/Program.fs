c03db33921040a09f803154df557c9f3bbd20870
Kevin Schneider <schneider.kev@outlook.de>
2023-12-13 15:05:11 +0100

    Hotfix: prevent complete pipeline failure by catching internal exceptions and writing empty test results.

--- src/arc-validate/Program.fs
+++ src/arc-validate/Program.fs
@@ -7,12 +7,23 @@ open ArcValidation
 open ArcValidation.Configs
 open ExitCodes
 
+    
+let createInternalFailDummyTestResults () = 
+    testList "arc-validate" [
+        testCase "arc-validate" (fun _ -> 
+            Expect.isFalse true """arc-validate failed due to an internal error
+This error did likely NOT occur due to user input.
+An empty test result file will be created to reflect this and prevent the subsequent validation pipeline from failing.
+"""
+        )
+    ]
+    |> performTest
+
 
 [<EntryPoint>]
 let main argv =
     try
         let args = CLIArgs.cliArgParser.ParseCommandLine()
-
         let arcConfig = 
             args.TryGetResult(CLIArgs.ARC_Directory)
             |> Option.defaultValue (System.Environment.GetEnvironmentVariable("ARC_PATH")) // default to ARC_PATH if argument is not provided
@@ -24,42 +35,57 @@ let main argv =
             |> Option.defaultValue arcConfig.PathConfig.ArcRootPath
 
         let verbose = args.TryGetResult(CLIArgs.Verbose).IsSome
-
-        /// these tests MUST pass for an ARC to be considered for publishing
-        let criticalTests =
-            testList "Critical" [
-                TestGeneration.Critical.Arc.FileSystem.generateArcFileSystemTests arcConfig
-                TestGeneration.Critical.Arc.ISA.generateISATests arcConfig
-            ]
-
-        let criticalTestResults =
-            criticalTests
-            |> performTest
-
-        if criticalTestResults.failed |> List.isEmpty && criticalTestResults.errored |> List.isEmpty then // if no critical tests failed or errored
-            
-            /// these tests SHOULD pass for an ARC to be considered of high quality
-            let nonCriticalTests =
-                testList "Non-critical" [
-                    TestGeneration.NonCritical.Arc.ISA.generateISATests arcConfig
+        try
+            /// these tests MUST pass for an ARC to be considered for publishing
+            let criticalTests =
+                testList "Critical" [
+                    TestGeneration.Critical.Arc.FileSystem.generateArcFileSystemTests arcConfig
+                    TestGeneration.Critical.Arc.ISA.generateISATests arcConfig
                 ]
 
-            let nonCriticalTestResults =
-                nonCriticalTests
+            let criticalTestResults =
+                criticalTests
                 |> performTest
 
-            [criticalTestResults; nonCriticalTestResults] 
-            |> combineTestRunSummaries // aggregate critical and non-critical test results
-            |> writeJUnitSummary verbose (Path.Combine(outPath, "arc-validate-results.xml")) // write the combined result to a single file
+            if criticalTestResults.failed |> List.isEmpty && criticalTestResults.errored |> List.isEmpty then // if no critical tests failed or errored
+            
+                /// these tests SHOULD pass for an ARC to be considered of high quality
+                let nonCriticalTests =
+                    testList "Non-critical" [
+                        TestGeneration.NonCritical.Arc.ISA.generateISATests arcConfig
+                    ]
 
-            ExitCode.Success |> int // critical tests passed, non-critical tests have been performed. Success!
+                let nonCriticalTestResults =
+                    nonCriticalTests
+                    |> performTest
 
-        else // one or more critical tests failed or errored.
-            criticalTestResults
-            |> Expecto.writeJUnitSummary verbose (Path.Combine(outPath, "arc-validate-results.xml"))
+                [criticalTestResults; nonCriticalTestResults] 
+                |> combineTestRunSummaries // aggregate critical and non-critical test results
+                |> writeJUnitSummary verbose (Path.Combine(outPath, "arc-validate-results.xml")) // write the combined result to a single file
 
-            ExitCode.CriticalTestFailure |> int
+                ExitCode.Success |> int // critical tests passed, non-critical tests have been performed. Success!
 
+            else // one or more critical tests failed or errored.
+                criticalTestResults
+                |> Expecto.writeJUnitSummary verbose (Path.Combine(outPath, "arc-validate-results.xml"))
+
+                ExitCode.CriticalTestFailure |> int
+        with
+            | ex -> 
+                printfn "arc-validate failed due to an internal error."
+                printfn "This error did likely NOT occur due to user input."
+                printfn "An empty test result file will be created to reflect this and prevent the validation pipeline from failing."
+                
+                if verbose then
+                    printfn "%s" ex.Message
+                    printfn "%A" ex.StackTrace // might want to add verbosity level to hide this
+                else
+                    printfn "Run arc-validate with --verbose to see the full error message."
+
+                createInternalFailDummyTestResults()
+                |> Expecto.writeJUnitSummary false (Path.Combine(outPath, "arc-validate-results.xml"))
+
+                ExitCode.InternalError |> int
     with
         | :? ArguParseException as ex ->
             match ex.ErrorCode with
@@ -74,9 +100,15 @@ let main argv =
                 ExitCode.ArgParseError |> int
 
             | _ -> 
-                printfn "Internal Error:"
+                printfn "arc-validate failed due to an internal error."
+                printfn "This error did likely NOT occur due to user input."
+                printfn "An empty test result file will be created to reflect this and prevent the validation pipeline from failing."
                 printfn "%s" ex.Message
                 printfn "%A" ex.StackTrace // might want to add verbosity level to hide this
+
+                createInternalFailDummyTestResults()
+                |> Expecto.writeJUnitSummary false ("arc-validate-results.xml")
+
                 ExitCode.InternalError |> int
         | ex ->
             printfn "Internal Error:"
