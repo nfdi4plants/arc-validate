734ec8033bcc6377f9d3d1c10b1b027183b98e71
Oliver Maus <oliver.maus@eclipso.de>
2024-01-11 18:14:31 +0100

    Add second iteration of Invenio prototypic script (WIP)

---
+++ playgrounds/qcPackage_prototypes/invenio_prototype_v0.2.0.fsx
@@ -0,0 +1,63 @@
+#I "../../src/arc-validate/bin/Debug/net8.0"
+#I "../../src/arc-validate/bin/Release/net8.0"
+#r "ARCExpect.dll"
+#r "ARCTokenization.dll"
+#r "ControlledVocabulary.dll"
+#r "arc-validate.dll"
+#r "Anybadge.NET.dll"
+#r "Expecto.dll"
+#r "OBO.NET.dll"
+
+//#r "nuget: ARCExpect"
+#r "nuget: Anybadge.NET"
+#r "nuget: FSharpAux"
+#r "nuget: Graphoscope"
+#r "nuget: Cytoscape.NET"
+
+open ARCExpect
+open ARCTokenization
+open ARCTokenization.StructuralOntology
+open ControlledVocabulary
+open Expecto
+open Expecto.Tests
+open AnyBadge.NET
+open ARCValidate
+open FSharpAux
+open Graphoscope
+open Cytoscape
+
+open System.IO
+
+
+// input:
+
+let arcDir = @"C:\Repos\git.nfdi4plants.org\ArcPrototype"
+let outDir = arcDir
+
+
+// values:
+
+let absoluteDirectoryPaths = FileSystem.parseRelativeDirectoryPaths arcDir
+let absoluteFilePaths = FileSystem.parseRelativeFilePaths arcDir
+let invFileTokens = 
+    Investigation.parseMetadataSheetsFromTokens() absoluteFilePaths 
+    |> List.concat
+    |> ARCGraph.fillTokenList Terms.InvestigationMetadata.ontology
+    |> Seq.concat
+    |> Seq.concat
+
+
+// Validation Cases:
+
+let cases = [
+    testList INVMSO.``Investigation Metadata``.INVESTIGATION.key.Name [
+        ARCExpect.validate (TestID.Name INVMSO.``Investigation Metadata``.INVESTIGATION.``Investigation Title``.Name) {
+            invFileTokens
+            |> Validate.ByTerm.contains INVMSO.``Investigation Metadata``.INVESTIGATION.``Investigation Title``
+        }
+        ARCExpect.validate (TestID.Name INVMSO.``Investigation Metadata``.INVESTIGATION.``Investigation Description``.Name) {
+            cvParams
+            |> Validate.ByTerm.contains INVMSO.``Investigation Metadata``.INVESTIGATION.``Investigation Description``
+        }
+    ]
+]
