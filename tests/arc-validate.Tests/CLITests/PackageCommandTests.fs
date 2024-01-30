342cc987d171aa3894c08cae40608af7952015bb
Kevin Schneider <schneider.kev@outlook.de>
2024-01-30 10:29:40 +0100

    wip package cli tests

--- tests/arc-validate.Tests/CLITests/PackageCommandTests.fs
+++ tests/arc-validate.Tests/CLITests/PackageCommandTests.fs
@@ -16,9 +16,22 @@ open JUnit
 [<Tests>]
 let ``PackageCommand CLI Tests`` =
     testSequenced (testList "arc-validate package" [
+        testSequenced (testList "list" [
+            yield! 
+                testFixture (Fixtures.withToolExecution 
+                    true
+                    "../../../../../publish/arc-validate" 
+                    [|"-v"; "package"; "list";|]
+                    (get_gh_api_token())
+                ) [
+                    "Exit code is 0" , 
+                        fun tool args proc -> Expect.equal proc.ExitCode 0 $"""incorrect exit code.{System.Environment.NewLine}{proc.Result.Output} (tool: {tool} args: {args |> String.concat " "})"""
+                ]
+        ])
         testSequenced (testList "install" [
             yield! 
                 testFixture (Fixtures.withToolExecution 
+                    true
                     "../../../../../publish/arc-validate" 
                     [|"-v"; "package"; "install"; "test"|]
                     (get_gh_api_token())
@@ -33,8 +46,58 @@ let ``PackageCommand CLI Tests`` =
                         fun tool args proc -> Expect.isTrue (File.Exists(Path.Combine(expected_package_cache_folder_path, "test.fsx"))) $"""package file was not installed at expected location (tool: {tool} args: {args |> String.concat " "})"""
                 ]
         ])
-
-        testSequenced (testList "update" [])
-        testSequenced (testList "delete" [])
-        testSequenced (testList "update-index" [])
+        testSequenced (testList "list" [
+            yield! 
+                testFixture (Fixtures.withToolExecution 
+                    false
+                    "../../../../../publish/arc-validate" 
+                    [|"-v"; "package"; "list";|]
+                    (get_gh_api_token())
+                ) [
+                    "package is listed after install" , 
+                        fun tool args proc -> Expect.equal proc.ExitCode 0 $"""incorrect exit code.{System.Environment.NewLine}{proc.Result.Output} (tool: {tool} args: {args |> String.concat " "})"""
+                ]
+        ])
+        testSequenced (testList "uninstall" [
+            yield! 
+                testFixture (Fixtures.withToolExecution 
+                    false
+                    "../../../../../publish/arc-validate" 
+                    [|"-v"; "package"; "uninstall"; "test"|]
+                    (get_gh_api_token())
+                ) [
+                    "Exit code is 0" , 
+                        fun tool args proc -> Expect.equal proc.ExitCode 0 $"""incorrect exit code.{System.Environment.NewLine}{proc.Result.Output} (tool: {tool} args: {args |> String.concat " "})"""
+                    "Cache folder still exists" ,  
+                        fun tool args proc -> Expect.isTrue (Directory.Exists(expected_package_cache_folder_path)) $"""package cache folder was not created at {expected_package_cache_folder_path} (tool: {tool} args: {args |> String.concat " "})"""
+                    "Cache exists" ,  
+                        fun tool args proc -> Expect.isTrue (File.Exists(expected_package_cache_file_path)) $"""package cache was not created at {expected_package_cache_file_path} (tool: {tool} args: {args |> String.concat " "})"""
+                    "Package script exists" ,  
+                        fun tool args proc -> Expect.isTrue (File.Exists(Path.Combine(expected_package_cache_folder_path, "test.fsx"))) $"""package file was not installed at expected location (tool: {tool} args: {args |> String.concat " "})"""
+                ]
+        ])
+        testSequenced (testList "list" [
+            yield! 
+                testFixture (Fixtures.withToolExecution 
+                    false
+                    "../../../../../publish/arc-validate" 
+                    [|"-v"; "package"; "list";|]
+                    (get_gh_api_token())
+                ) [
+                    "package is not listed anymore after install" , 
+                        fun tool args proc -> Expect.equal proc.ExitCode 0 $"""incorrect exit code.{System.Environment.NewLine}{proc.Result.Output} (tool: {tool} args: {args |> String.concat " "})"""
+                ]
+        ])
+        testSequenced (testList "update-index" [
+            yield! 
+                testFixture (Fixtures.withToolExecution 
+                    false
+                    "../../../../../publish/arc-validate" 
+                    [|"-v"; "package"; "update-index"|]
+                    (get_gh_api_token())
+                ) [
+                    "Exit code is 0" , 
+                        fun tool args proc -> Expect.equal proc.ExitCode 0 $"""incorrect exit code.{System.Environment.NewLine}{proc.Result.Output} (tool: {tool} args: {args |> String.concat " "})"""
+                ]
+        ])
     ])
--- tests/arc-validate.Tests/TestUtils.fs
+++ tests/arc-validate.Tests/TestUtils.fs
@@ -10,11 +10,15 @@ open ReferenceObjects
 
 module Fixtures =
 
-    let withToolExecution (tool:string) (args: string []) (token: string option) (f: string -> string[] -> ProcessResult<ProcessOutput> -> unit) () =
-        resetConfigEnvironment()
+    let withToolExecution (resetEnv:bool) (tool:string) (initialArgs: string []) (token: string option) (f: string -> string[] -> ProcessResult<ProcessOutput> -> unit) () =
+        if resetEnv then resetConfigEnvironment()
         let args = 
             match token with
-            | Some t -> Array.append [| "-t"; t |] args
-            | None -> args
+            | Some t -> Array.append [| "-t"; t |] initialArgs
+            | None -> initialArgs
+        let displayArgs = // let's not leak credentials in error message here
+            match token with
+                | Some t -> Array.append [| "-t"; "<API_TOKEN_WAS_SET>" |] initialArgs
+                | None -> initialArgs
         let result = runTool tool args
-        f tool args result
+        f tool displayArgs result
