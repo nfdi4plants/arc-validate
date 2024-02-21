bd4d2de49ef3f09fa68c475d47258ab05590e028
omaus <oliver.maus@eclipso.de>
2024-02-21 19:01:43 +0100

    Add unit test for ErrorMessage (WIP)

---
+++ tests/ARCExpect.Tests/ErrorMessageTests.fs
@@ -0,0 +1,19 @@
+﻿module ErrorMessageTests
+
+
+open Expecto
+open ARCExpect
+open ControlledVocabulary
+
+
+let dummyIParam = CvParam("test:0", "testTerm", "test", ParamValue.Value "no val")
+dummyIParam.AddAttribute(CvParam())
+
+
+[<Tests>]
+let ``ErrorMessage tests`` =
+    testList "ErrorMessage" [
+        testList "ofIParamCollection" [
+            testCase "resolves correctly"
+        ]
+    ]
