1a35b0e5d8ef5a229011e0d07c135ffa2c1d670a
Christopher Lux <lux-christopher@web.de>
2024-04-22 15:43:14 +0200

    Add test for ARCSpecification V2_Draft
    Add test for ARCSpecification V2_Draft.

---
+++ tests/arc-validate.Tests/ARCSpecification.fs
@@ -0,0 +1,27 @@
+﻿module ARCSpecification
+
+open Expecto
+open System.IO
+open ARCTokenization
+
+
+[<Tests>]
+let tests =
+    testList "ARCValidationPackage tests" [
+        test "V2_Draft" {
+
+            let testPath = System.IO.Path.GetFullPath("Fixtures/arcs/testingArc/")
+
+            let cases = ARCValidate.ARCSpecification.V2_Draft.testCases testPath
+
+            
+            let runSummery = ARCExpect.Execute.Validation cases
+
+            //false None (ParseResults<ARCValidate.CLIArguments.ValidateArgs>.Empty)
+            Expect.isTrue
+                runSummery.successful
+                "ARCValidationPackage cache date was not updated correctly."
+            
+        }
+       
+    ]
