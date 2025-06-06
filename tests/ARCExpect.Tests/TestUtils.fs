1f0b8c4eb0fe75522f4f46cbe4dbe9efaba5f23a
Kevin Schneider <schneider.kev@outlook.de>
2025-06-06 10:04:14 +0200

    #140: Add payload option to validation summary

--- tests/ARCExpect.Tests/TestUtils.fs
+++ tests/ARCExpect.Tests/TestUtils.fs
@@ -22,4 +22,15 @@ module Expect =
                 Critical.OriginalRunSummary = None
                 NonCritical.OriginalRunSummary = None
             }
-        Expect.equal actual expected "validation summaries were not equal ignoring the original run summaries."
+        Expect.equal actual.Critical expected.Critical "Critical Field not equal. validation summaries were not equal ignoring the original run summaries."
+        Expect.equal actual.NonCritical expected.NonCritical "NonCritical Field not equal. validation summaries were not equal ignoring the original run summaries."
+        Expect.equal actual.ValidationPackage expected.ValidationPackage "Critical Field not equal. validation summaries were not equal ignoring the original run summaries."
+        if actual.Payload.IsSome then
+            if expected.Payload.IsSome then 
+                Expect.sequenceEqual (actual.Payload.Value.Keys) (expected.Payload.Value.Keys) "Payload not equal (Keys)"
+                Expect.sequenceEqual (actual.Payload.Value.Keys) (expected.Payload.Value.Keys) "Payload not equal (Values)"
+            else
+                failwith "actual had Some Payload, but expected did not."
+        else
+            if expected.Payload.IsSome then 
+                failwith "expected had Some Payload, but actual did not."
